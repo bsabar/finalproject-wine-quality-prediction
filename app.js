a63ded3e30a10b0c4ded0d86df33deb80a58c229
Karina Quintana <kquintana031@gmail.com>
2023-08-20 15:20:58 -0600

    try to fix html pending items

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const description1 = document.getElementById('description1');
 const imageElement2 = document.getElementById('imageElement2');
 const description2 = document.getElementById('description2');
 
-// You can change the image sources and descriptions as needed
+// Image sources and descriptions as needed
 imageElement1.src = '/Resources/feature_importance.png';
-description1.textContent = 'Description for Image 1';
+description1.textContent = 'In determining the needed elements for wine color impact, the feature importance outlines the impact by order of color feature. ';
 
 imageElement2.src = '/Resources/confusion_matrix_tf.png';
 description2.textContent = 'Description for Image 2';
